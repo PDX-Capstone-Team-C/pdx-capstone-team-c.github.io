1f9d712f2c150ba044da950881b7281c1da50f15
Max Siegfried <mjsiegfried@gmail.com>
2015-10-25 10:01:24 -0700

    fixed some logic

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -3,17 +3,18 @@ $(window).scroll(function () {
     var fading = $('#team').find('img')
     fading.each(function () {
         var offset = $(this).offset()
+
         var positionTop = offset.top - $(window).scrollTop();
 
         var offsetBottom = $(document).height() - $(this).height() - offset.top;
-
         var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-        var positionBottom = scrollBottom - offsetBottom
-
-        positionBottom = Math.abs(positionBottom)
+        var positionBottom = offsetBottom - scrollBottom
 
         var big = Math.max(positionTop, positionBottom)
         var small = Math.min(positionTop, positionBottom)
+
+        console.log(positionTop)
+        console.log(positionBottom)
 
         var opacity = small / big + 0.25
         $(this).fadeTo(0, opacity)
