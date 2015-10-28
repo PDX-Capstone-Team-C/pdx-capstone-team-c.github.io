1db61656aba5769a197868a2b31a45c48e2bd3d7
Trevor Eyre <trevoreyre@gmail.com>
2015-10-27 18:34:38 -0700

    Fix for jQuery scroll event on mobile

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -1,10 +1,17 @@
 $(window).scroll(function () {
+    scrollHandler(96);
+});
+$('body').on('touchmove', function () {
+    scrollHandler(16);
+});
+
+function scrollHandler (offsetPx) {
     var scrollBottom = $(window).scrollTop() + $(window).height();
     var fadeInElements = $('.csc-fade-in');
 
     fadeInElements.each(function () {
-        if (scrollBottom > ($(this).offset().top + 96)) {
+        if (scrollBottom > ($(this).offset().top + offsetPx)) {
             $(this).addClass('animated fadeInUp');
         }
-    });
-});
+    });    
+}
